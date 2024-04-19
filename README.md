db8f1848b31b7d01f17fa2a2eec09a3b3ee695ef
maj-dan <160074910+maj-dan@users.noreply.github.com>
2024-04-19 10:05:32 -0300

    Write initial game pseudocode

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rock-paper-scissors
 
 This is a rock paper scissors game, played in the browser's console.
+The player is going to play against the computer.
---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Create player score and computer score
+//Run 5 rounds of the game
+    //Ask player for an choice (rock, paper or scissors)
+    //if player choice is valid, store it, else ask for choice again
+    //Get computer choice
+    //Store computer choice
+    //Compare player and computer choice
+        //if player win, output message "Player +1!" and add 1 to player score
+        //if computer win, output message "Computer +1!" add 1 to computer score
+        //if it's a tie, output message "Tie!"
+//Compare player score and computer score
+    //if player score is greater, output message "Player win!"
+    //if computer score is greater, output message "Computer win!"
+    //if it's equal, output message "It's a tie!"
