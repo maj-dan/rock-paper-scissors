7ffd722e88843a6321669ed5a89fe07b527a4c66
maj-dan <160074910+maj-dan@users.noreply.github.com>
2024-05-02 14:28:52 -0300

    Add buttons to page and store an reference of them

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
+const btns = document.querySelectorAll("button");
 
-//Run 5 rounds of the game
-function playGame() {
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++){
-        //if player win, output message "Player +1!" and add 1 to player score
-        //if computer win, output message "Computer +1!" dec 1 to player score
-        //if it's a tie, output message "Tie!"
-        playerScore += playRound(getPlayerChoice(), getComputerChoice());
-    }
-    //if player score is greater than 0, output message "Player win!"
-    //if player score is lesser than 0, output message "Computer win!"
-    //if it's 0, output message "It's a tie!"
-    if (playerScore > 0) {
-        console.log("Player win!");
-    } else if (playerScore < 0) {
-        console.log("Computer win!");
-    } else {
-        console.log("It's a tie!");
-    }
-}
+btns.forEach((btn) => btn.addEventListener("click", playRound));
 
 //Get computer choice
 function getComputerChoice() {
@@ -75,14 +57,4 @@ function playRound(playerSelection, computerSelection) {
                 return 0;
         }
     }
-}
-
-//Ask player for an choice (rock, paper or scissors)
-//if player choice is valid, store it, else ask for choice again
-function getPlayerChoice() {
-    let playerChoice;
-    do {
-        playerChoice = prompt("Enter Rock, Paper or Scissors:");
-    } while (playerChoice.toUpperCase()!== "ROCK" && playerChoice.toUpperCase()!== "PAPER" && playerChoice.toUpperCase()!== "SCISSORS");
-    return playerChoice;
 }
