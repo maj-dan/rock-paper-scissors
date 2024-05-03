00f6d0ae83dde41427efbcbbc51635e1055bc239
maj-dan <160074910+maj-dan@users.noreply.github.com>
2024-05-03 10:11:03 -0300

    Display score

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const btns = document.querySelectorAll("button");
 const display = document.querySelector("div#display");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
 
 btns.forEach((btn) => btn.addEventListener("click", playRound));
 
@@ -15,7 +17,6 @@ function getComputerChoice() {
     }
 }
 
-
 function playRound(event) {
     computerSelection = getComputerChoice();
     playerSelection = event.target.textContent.toUpperCase();
@@ -23,37 +24,54 @@ function playRound(event) {
         switch (computerSelection) {
             case "ROCK":
                 display.textContent = "Rock = Rock, Tie!";
-                return 0;
+                updateScore(0);
+                break;
             case "PAPER":
                 display.textContent = "Rock < Paper, Computer +1!";
-                return -1;
+                updateScore(-1);
+                break;
             case "SCISSORS":
                 display.textContent = "Rock > Scissors, Player +1!";
-                return 1;
+                updateScore(1);
+                break;
         }
     } else if (playerSelection === "PAPER") {
         switch (computerSelection) {
             case "ROCK":
                 display.textContent = "Paper > Rock, Player +1!";
-                return 1;
+                updateScore(1);
+                break;
             case "PAPER":
                 display.textContent = "Paper = Paper, Tie!";
-                return 0;
+                updateScore(0);
+                break;
             case "SCISSORS":
                 display.textContent = "Paper < Scissors, Computer +1!";
-                return -1;
+                updateScore(-1);
+                break;
         }
     } else if (playerSelection === "SCISSORS") {
         switch (computerSelection) {
             case "ROCK":
                 display.textContent = "Scissors < Rock, Computer +1!";
-                return -1;
+                updateScore(-1);
+                break;
             case "PAPER":
                 display.textContent = "Scissors > Paper, Player +1!";
-                return 1;
+                updateScore(1);
+                break;
             case "SCISSORS":
                 display.textContent = "Scissors = Scissors, Tie!";
-                return 0;
+                updateScore(0);
+                break;
         }
+    }
+}
+
+function updateScore(roundResult) {
+    if (roundResult > 0){
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+    }else if (roundResult < 0){
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
     }
 }
